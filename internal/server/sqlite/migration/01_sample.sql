3403569dc48055d8d04a58eb004f0d88c940d016
Maxim Kolibaba <avierdaner@gmail.com>
2025-09-27 13:00:31 +0300

    Update server sqlite

---
+++ internal/server/sqlite/migration/01_sample.sql
@@ -0,0 +1,19 @@
+INSERT INTO login (name, login, password, metadata, user)
+VALUES ('Google', 'iivanov', '123456', null, 'demo'),
+       ('Ozon', '79031002030', 'qwe123', null, 'demo'),
+       ('Wildberries', '79031002030', 'qwerty', null, 'demo'),
+       ('Госуслуги', 'iivanov@gmail.com', 'qweasd', null, 'demo'),
+       ('Mail', 'ivanivanov', 'qqwwee', null, 'demo'),
+       ('VK', 'ivanivanov@mail.ru', 'qweqwe', null, 'demo');
+
+INSERT INTO note (name, text, metadata, user)
+VALUES ('Записки о природе',
+        'Кто никогда не видал, как растет клюква, тот может очень долго идти по болоту и не замечать, что он по клюкве идет.',
+        null, 'demo'),
+       ('Мысль',
+        'Живешь ты, может быть, сам триста лет, и кто породил тебя, тот в яичке своем пересказал все, что он тоже узнал за свои триста лет жизни.',
+        null, 'demo');
+
+INSERT INTO card (name, number, exp_date, cvv, cardholder, metadata, user)
+VALUES ('Сбербанк', '2200123456789019', '03/28', '541', 'IVAN IVANOV', null, 'demo'),
+       ('Т-Банк', '2201987654321000', '01/29', '192', 'IVAN IVANOV', null, 'demo');
