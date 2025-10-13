a212d5226572fe0449cebf3c4facbd51d716ade5
Maxim Kolibaba <avierdaner@gmail.com>
2025-10-13 23:53:16 +0300

    Add live reload

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"github.com/charmbracelet/log"
 	"github.com/mkolibaba/gophkeeper/internal/client"
 	"github.com/mkolibaba/gophkeeper/internal/client/grpc"
 	"github.com/mkolibaba/gophkeeper/internal/client/inmem"
 	"github.com/mkolibaba/gophkeeper/internal/client/tui"
 	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
+	"log/slog"
 )
 
 func main() {
@@ -14,6 +17,9 @@ func main() {
 
 func createApp() fx.Option {
 	return fx.Options(
+		fx.WithLogger(func(logger *log.Logger) fxevent.Logger {
+			return &fxevent.SlogLogger{Logger: slog.New(logger)}
+		}),
 		client.Module,
 		grpc.Module,
 		inmem.Module,
