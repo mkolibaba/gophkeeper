afe69fdbc1e9c529ec614452b1eddc3d035f5c21
Maxim Kolibaba <avierdaner@gmail.com>
2025-10-27 13:09:34 +0300

    Add client tui test coverage

--- client/tui/tuitest/bubble_test.go
+++ client/tui/tuitest/bubble_test.go
@@ -148,7 +148,7 @@ func TestAddDataView(t *testing.T) {
 			strings.Contains(s, "Detail")
 	})
 
-	// Заходим в форму добавления логина.
+	// Заходим в форму добавления.
 	tm.Send(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune{'2'}, Alt: true})
 	waitFor(t, tm, func(s string) bool {
 		return strings.Contains(s, "Add Note")
@@ -170,6 +170,92 @@ func TestAddDataView(t *testing.T) {
 	c := cc[0].Data
 	require.Equal(t, c.Name, "new name")
 	require.Equal(t, c.Text, "some long long long text")
+}
+
+func TestEditDataView(t *testing.T) {
+	t.Parallel()
+
+	userService := inmem.NewUserService(log.New(io.Discard))
+	authMock := &mock.AuthorizationServiceMock{
+		AuthorizeFunc: func(ctx context.Context, login string, password string) (string, error) {
+			return "some token", nil
+		},
+	}
+	noteServiceMock := &mock.NoteServiceMock{
+		GetAllFunc: func(ctx context.Context) ([]client.NoteData, error) {
+			return []client.NoteData{{
+				ID:   1,
+				Name: "my note",
+				Text: "long text",
+			}}, nil
+		},
+		SaveFunc: func(ctx context.Context, data client.NoteData) error {
+			return nil
+		},
+	}
+	var config client.Config
+	config.Development.Enabled = false
+
+	bubble, err := tui.NewBubble(tui.BubbleParams{
+		Config: &config, // TODO: выглядит как сильная связанность
+		AuthorizationView: authorization.New(authorization.Params{
+			AuthorizationService: authMock,
+			UserService:          userService,
+		}),
+		MainView: home.New(home.Params{
+			LoginService:  &mock.LoginServiceMock{},
+			BinaryService: &mock.BinaryServiceMock{},
+			NoteService:   noteServiceMock,
+			CardService:   &mock.CardServiceMock{},
+			UserService:   userService,
+		}),
+		AddDataView: adddata.New(adddata.Params{}),
+		EditDataView: editdata.New(editdata.Params{
+			NoteService: noteServiceMock,
+		}),
+		RegistrationView: registration.New(registration.Params{}),
+	})
+	require.NoError(t, err)
+
+	// Инициализируем приложение.
+	tm := teatest.NewTestModel(t, bubble, teatest.WithInitialTermSize(130, 40))
+
+	// Ожидаем отрисовки формы авторизации.
+	waitFor(t, tm, func(s string) bool {
+		return strings.Contains(s, "Authorization") &&
+			strings.Contains(s, "Login") &&
+			strings.Contains(s, "Password")
+	})
+
+	// За счет мока сразу авторизуемся.
+	tm.Send(tea.KeyMsg{Type: tea.KeyEnter})
+
+	// Проверяем, что приложение перешло на домашнюю страницу и отрисовало таблицу.
+	waitFor(t, tm, func(s string) bool {
+		return strings.Contains(s, "Data") &&
+			strings.Contains(s, "Detail") &&
+			strings.Contains(s, "my note")
+	})
+
+	// Заходим в форму редактирования.
+	tm.Send(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune{'e'}, Alt: true})
+	waitFor(t, tm, func(s string) bool {
+		return strings.Contains(s, "Edit my note")
+	})
+
+	// Заполняем форму.
+	tm.Type(" new new")
+	tm.Send(tea.KeyMsg{Type: tea.KeyCtrlS})
+	waitFor(t, tm, func(s string) bool {
+		return strings.Contains(s, "Data") &&
+			strings.Contains(s, "Detail")
+	})
+
+	// Проверяем корректность переданных данных
+	cc := noteServiceMock.UpdateCalls()
+	require.Len(t, cc, 1)
+	c := cc[0].Data
+	require.Equal(t, *c.Name, "my note new new")
 }
 
 func waitFor(t *testing.T, tm *teatest.TestModel, cond func(s string) bool) {
